1c69b21528cd57455393cb1752c848ba9b5cba6e
olegsvekla993 <olegsvekla993@gmail.com>
2023-01-16 16:31:37 +0300

    add some inf to controller, add MVC to project, add view for xample method

--- MVCForTraining/MVCForTraining/Controllers/HomeController.cs
+++ MVCForTraining/MVCForTraining/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MVCForTraining.Models;
 using System.Diagnostics;
+using System.Xml.Linq;
 
 namespace MVCForTraining.Controllers
 {
@@ -13,14 +14,38 @@ namespace MVCForTraining.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task Index()
         {
-            return View();
+            string content = @"<form method='post' action='/Home/PersonData'>
+                   <label>Name:</label><br />
+                <input name='name' /><br />
+                <label>Age:</label><br />
+                <input type='number' name='age' /><br />
+                <input type='submit' value='Send' />
+            </form>";
+            Response.ContentType = "text/html;charset=utf-8";
+            await Response.WriteAsync(content);
         }
+        [HttpPost]
+        public string PersonData()
+        {
+            string name = Request.Form["name"];
+            string age = Request.Form["age"];
+            return $"{name}: {age}";
+        }
+
 
         public IActionResult Privacy()
         {
             return View();
+        }
+       
+
+        public string Example()
+        {
+            string name = Request.Query["name"];
+            string age = Request.Query["age"];
+            return $"Name: {name}  Age: {age}";
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
