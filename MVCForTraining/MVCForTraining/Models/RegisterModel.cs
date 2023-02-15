23beb4bebcc70357e71b97d4c3069c1a59f28893
olegsvekla993 <olegsvekla993@gmail.com>
2023-02-15 10:42:14 +0300

    add UserRole class, add props to RegisterModel and to improve appsettings.json

--- MVCForTraining/MVCForTraining/Models/RegisterModel.cs
+++ MVCForTraining/MVCForTraining/Models/RegisterModel.cs
@@ -1,8 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVCForTraining.Models
 {
-    public class RegisterModel
+    public class RegisterModel 
     {
         [Required(ErrorMessage = "Enter a name")]
         [MaxLength(20, ErrorMessage = "The name must be less than 20 characters long")]
@@ -12,6 +13,8 @@ namespace MVCForTraining.Models
         public string Username { get; set; }
 
         public string ConfirmEmail { get; set; }
+
+        public UserRole Role { get; set; }
 
         [DataType(DataType.Password)]
         [Required(ErrorMessage = "Enter a password")]
---
+++ MVCForTraining/MVCForTraining/Models/UserRole.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MVCForTraining.Models
+{
+    public class UserRole : IdentityRole
+    {
+        public IdentityRole Role
+
+        {
+               // возвращаем значение свойства
+            set
+            {
+                Name = "User";   // устанавливаем новое значение свойства
+            }
+        }
+
+        public UserRole(IdentityRole role)
+        {
+            Role = role;
+        }
+    }
+}
